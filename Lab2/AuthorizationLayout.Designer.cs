5ba96fc92d0f68b94d4a58197d7e9da6c9434de1
Aleego <Sankafoxe@gmail.com>
2025-02-27 08:06:43 +0300

    add lab2

---
+++ Lab2/AuthorizationLayout.Designer.cs
@@ -0,0 +1,133 @@
+﻿using System.ComponentModel;
+
+namespace Lab2;
+
+partial class AuthorizationLayout
+{
+    /// <summary> 
+    /// Required designer variable.
+    /// </summary>
+    private IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    #region Component Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        panel_authorization = new System.Windows.Forms.Panel();
+        btn_accept = new System.Windows.Forms.Button();
+        lb_title = new System.Windows.Forms.Label();
+        tb_username = new System.Windows.Forms.TextBox();
+        lb_username = new System.Windows.Forms.Label();
+        panel_authorization.SuspendLayout();
+        SuspendLayout();
+        // 
+        // panel_authorization
+        // 
+        panel_authorization.BackColor = System.Drawing.Color.FromArgb(((int)((byte)64)), ((int)((byte)64)), ((int)((byte)64)));
+        panel_authorization.Controls.Add(btn_accept);
+        panel_authorization.Controls.Add(lb_title);
+        panel_authorization.Controls.Add(tb_username);
+        panel_authorization.Controls.Add(lb_username);
+        panel_authorization.Dock = System.Windows.Forms.DockStyle.Fill;
+        panel_authorization.Location = new System.Drawing.Point(0, 0);
+        panel_authorization.Margin = new System.Windows.Forms.Padding(0);
+        panel_authorization.MinimumSize = new System.Drawing.Size(225, 254);
+        panel_authorization.Name = "panel_authorization";
+        panel_authorization.Size = new System.Drawing.Size(225, 254);
+        panel_authorization.TabIndex = 0;
+        // 
+        // btn_accept
+        // 
+        btn_accept.Anchor = System.Windows.Forms.AnchorStyles.None;
+        btn_accept.BackColor = System.Drawing.Color.DimGray;
+        btn_accept.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+        btn_accept.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+        btn_accept.FlatAppearance.BorderSize = 0;
+        btn_accept.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)204));
+        btn_accept.ForeColor = System.Drawing.Color.White;
+        btn_accept.Location = new System.Drawing.Point(48, 161);
+        btn_accept.Margin = new System.Windows.Forms.Padding(0);
+        btn_accept.Name = "btn_accept";
+        btn_accept.Size = new System.Drawing.Size(136, 26);
+        btn_accept.TabIndex = 10;
+        btn_accept.Text = "Подтвердить";
+        btn_accept.UseVisualStyleBackColor = false;
+        btn_accept.Click += btn_accept_Click;
+        // 
+        // lb_title
+        // 
+        lb_title.Anchor = System.Windows.Forms.AnchorStyles.None;
+        lb_title.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)204));
+        lb_title.ForeColor = System.Drawing.Color.White;
+        lb_title.Location = new System.Drawing.Point(-11, -14);
+        lb_title.Name = "lb_title";
+        lb_title.Size = new System.Drawing.Size(247, 75);
+        lb_title.TabIndex = 9;
+        lb_title.Text = "Авторизация";
+        lb_title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // tb_username
+        // 
+        tb_username.Anchor = System.Windows.Forms.AnchorStyles.None;
+        tb_username.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)204));
+        tb_username.Location = new System.Drawing.Point(50, 112);
+        tb_username.Name = "tb_username";
+        tb_username.PlaceholderText = "пользователь";
+        tb_username.Size = new System.Drawing.Size(129, 25);
+        tb_username.TabIndex = 5;
+        // 
+        // lb_username
+        // 
+        lb_username.Anchor = System.Windows.Forms.AnchorStyles.None;
+        lb_username.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)204));
+        lb_username.ForeColor = System.Drawing.Color.White;
+        lb_username.Location = new System.Drawing.Point(-11, 80);
+        lb_username.Margin = new System.Windows.Forms.Padding(0);
+        lb_username.Name = "lb_username";
+        lb_username.Size = new System.Drawing.Size(247, 29);
+        lb_username.TabIndex = 6;
+        lb_username.Text = "Имя пользователя";
+        lb_username.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // AuthorizationLayout
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        BackColor = System.Drawing.Color.White;
+        Controls.Add(panel_authorization);
+        Margin = new System.Windows.Forms.Padding(0);
+        MinimumSize = new System.Drawing.Size(225, 254);
+        Size = new System.Drawing.Size(225, 254);
+        panel_authorization.ResumeLayout(false);
+        panel_authorization.PerformLayout();
+        ResumeLayout(false);
+    }
+
+    private System.Windows.Forms.Button btn_accept;
+
+    private System.Windows.Forms.Label lb_title;
+    private System.Windows.Forms.Label lb_username;
+    private System.Windows.Forms.TextBox tb_username;
+
+    private System.Windows.Forms.Panel panel_authorization;
+
+    #endregion
+}
---
+++ Lab2/ChatLayout.Designer.cs
@@ -0,0 +1,247 @@
+﻿using System.ComponentModel;
+
+namespace Lab2;
+
+partial class ChatLayout
+{
+    /// <summary> 
+    /// Required designer variable.
+    /// </summary>
+    private IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    #region Component Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        p_chat = new System.Windows.Forms.Panel();
+        p_topBar = new System.Windows.Forms.Panel();
+        tb_dialog = new System.Windows.Forms.TextBox();
+        p_bottomBar = new System.Windows.Forms.Panel();
+        btn_send = new System.Windows.Forms.Button();
+        tb_message = new System.Windows.Forms.TextBox();
+        p_rightBar = new System.Windows.Forms.Panel();
+        p_otherUsers = new System.Windows.Forms.Panel();
+        lb_users = new System.Windows.Forms.Label();
+        p_leaveButton = new System.Windows.Forms.Panel();
+        btn_leave = new System.Windows.Forms.Button();
+        p_userInfo = new System.Windows.Forms.Panel();
+        lb_info = new System.Windows.Forms.Label();
+        p_chat.SuspendLayout();
+        p_topBar.SuspendLayout();
+        p_bottomBar.SuspendLayout();
+        p_rightBar.SuspendLayout();
+        p_otherUsers.SuspendLayout();
+        p_leaveButton.SuspendLayout();
+        p_userInfo.SuspendLayout();
+        SuspendLayout();
+        // 
+        // p_chat
+        // 
+        p_chat.BackColor = System.Drawing.SystemColors.ButtonShadow;
+        p_chat.Controls.Add(p_topBar);
+        p_chat.Controls.Add(p_bottomBar);
+        p_chat.Controls.Add(p_rightBar);
+        p_chat.Dock = System.Windows.Forms.DockStyle.Fill;
+        p_chat.Location = new System.Drawing.Point(0, 0);
+        p_chat.Name = "p_chat";
+        p_chat.Size = new System.Drawing.Size(496, 462);
+        p_chat.TabIndex = 0;
+        // 
+        // p_topBar
+        // 
+        p_topBar.Controls.Add(tb_dialog);
+        p_topBar.Dock = System.Windows.Forms.DockStyle.Fill;
+        p_topBar.Location = new System.Drawing.Point(0, 0);
+        p_topBar.Margin = new System.Windows.Forms.Padding(0);
+        p_topBar.Name = "p_topBar";
+        p_topBar.Size = new System.Drawing.Size(325, 385);
+        p_topBar.TabIndex = 2;
+        // 
+        // tb_dialog
+        // 
+        tb_dialog.Dock = System.Windows.Forms.DockStyle.Fill;
+        tb_dialog.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)204));
+        tb_dialog.Location = new System.Drawing.Point(0, 0);
+        tb_dialog.Multiline = true;
+        tb_dialog.Name = "tb_dialog";
+        tb_dialog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+        tb_dialog.Size = new System.Drawing.Size(325, 385);
+        tb_dialog.TabIndex = 0;
+        // 
+        // p_bottomBar
+        // 
+        p_bottomBar.BackColor = System.Drawing.SystemColors.AppWorkspace;
+        p_bottomBar.Controls.Add(btn_send);
+        p_bottomBar.Controls.Add(tb_message);
+        p_bottomBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+        p_bottomBar.Location = new System.Drawing.Point(0, 385);
+        p_bottomBar.Margin = new System.Windows.Forms.Padding(0);
+        p_bottomBar.Name = "p_bottomBar";
+        p_bottomBar.Size = new System.Drawing.Size(325, 77);
+        p_bottomBar.TabIndex = 1;
+        // 
+        // btn_send
+        // 
+        btn_send.Anchor = System.Windows.Forms.AnchorStyles.None;
+        btn_send.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)204));
+        btn_send.Location = new System.Drawing.Point(262, 11);
+        btn_send.Name = "btn_send";
+        btn_send.Size = new System.Drawing.Size(52, 55);
+        btn_send.TabIndex = 1;
+        btn_send.Text = "send";
+        btn_send.UseVisualStyleBackColor = true;
+        btn_send.Click += btn_send_Click;
+        // 
+        // tb_message
+        // 
+        tb_message.Anchor = System.Windows.Forms.AnchorStyles.None;
+        tb_message.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)204));
+        tb_message.Location = new System.Drawing.Point(6, 10);
+        tb_message.Multiline = true;
+        tb_message.Name = "tb_message";
+        tb_message.Size = new System.Drawing.Size(250, 57);
+        tb_message.TabIndex = 0;
+        tb_message.KeyDown += tb_message_KeyDown;
+        // 
+        // p_rightBar
+        // 
+        p_rightBar.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+        p_rightBar.Controls.Add(p_otherUsers);
+        p_rightBar.Controls.Add(p_leaveButton);
+        p_rightBar.Controls.Add(p_userInfo);
+        p_rightBar.Dock = System.Windows.Forms.DockStyle.Right;
+        p_rightBar.Location = new System.Drawing.Point(325, 0);
+        p_rightBar.Margin = new System.Windows.Forms.Padding(0);
+        p_rightBar.Name = "p_rightBar";
+        p_rightBar.Size = new System.Drawing.Size(171, 462);
+        p_rightBar.TabIndex = 0;
+        // 
+        // p_otherUsers
+        // 
+        p_otherUsers.BackColor = System.Drawing.Color.Gainsboro;
+        p_otherUsers.Controls.Add(lb_users);
+        p_otherUsers.Dock = System.Windows.Forms.DockStyle.Fill;
+        p_otherUsers.Location = new System.Drawing.Point(0, 81);
+        p_otherUsers.Margin = new System.Windows.Forms.Padding(0);
+        p_otherUsers.Name = "p_otherUsers";
+        p_otherUsers.Size = new System.Drawing.Size(171, 324);
+        p_otherUsers.TabIndex = 2;
+        // 
+        // lb_users
+        // 
+        lb_users.Dock = System.Windows.Forms.DockStyle.Fill;
+        lb_users.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)204));
+        lb_users.Location = new System.Drawing.Point(0, 0);
+        lb_users.Name = "lb_users";
+        lb_users.Size = new System.Drawing.Size(171, 324);
+        lb_users.TabIndex = 0;
+        lb_users.Text = "Пользователи:";
+        lb_users.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+        // 
+        // p_leaveButton
+        // 
+        p_leaveButton.Controls.Add(btn_leave);
+        p_leaveButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+        p_leaveButton.Location = new System.Drawing.Point(0, 405);
+        p_leaveButton.Margin = new System.Windows.Forms.Padding(0);
+        p_leaveButton.Name = "p_leaveButton";
+        p_leaveButton.Size = new System.Drawing.Size(171, 57);
+        p_leaveButton.TabIndex = 1;
+        // 
+        // btn_leave
+        // 
+        btn_leave.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)204));
+        btn_leave.Location = new System.Drawing.Point(15, 11);
+        btn_leave.Name = "btn_leave";
+        btn_leave.Size = new System.Drawing.Size(145, 35);
+        btn_leave.TabIndex = 0;
+        btn_leave.Text = "leave";
+        btn_leave.UseVisualStyleBackColor = true;
+        // 
+        // p_userInfo
+        // 
+        p_userInfo.BackColor = System.Drawing.Color.Silver;
+        p_userInfo.Controls.Add(lb_info);
+        p_userInfo.Dock = System.Windows.Forms.DockStyle.Top;
+        p_userInfo.Location = new System.Drawing.Point(0, 0);
+        p_userInfo.Margin = new System.Windows.Forms.Padding(0);
+        p_userInfo.Name = "p_userInfo";
+        p_userInfo.Size = new System.Drawing.Size(171, 81);
+        p_userInfo.TabIndex = 0;
+        // 
+        // lb_info
+        // 
+        lb_info.Dock = System.Windows.Forms.DockStyle.Fill;
+        lb_info.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)204));
+        lb_info.Location = new System.Drawing.Point(0, 0);
+        lb_info.Name = "lb_info";
+        lb_info.Size = new System.Drawing.Size(171, 81);
+        lb_info.TabIndex = 0;
+        lb_info.Text = "Вы вошли как:";
+        lb_info.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+        // 
+        // ChatLayout
+        // 
+        BackColor = System.Drawing.SystemColors.ButtonHighlight;
+        Controls.Add(p_chat);
+        MinimumSize = new System.Drawing.Size(496, 462);
+        Size = new System.Drawing.Size(496, 462);
+        Load += ChatLayout_Load;
+        p_chat.ResumeLayout(false);
+        p_topBar.ResumeLayout(false);
+        p_topBar.PerformLayout();
+        p_bottomBar.ResumeLayout(false);
+        p_bottomBar.PerformLayout();
+        p_rightBar.ResumeLayout(false);
+        p_otherUsers.ResumeLayout(false);
+        p_leaveButton.ResumeLayout(false);
+        p_userInfo.ResumeLayout(false);
+        ResumeLayout(false);
+    }
+
+    private System.Windows.Forms.Button btn_leave;
+
+    private System.Windows.Forms.Button btn_send;
+
+    private System.Windows.Forms.TextBox tb_message;
+
+    private System.Windows.Forms.TextBox tb_dialog;
+
+    private System.Windows.Forms.Label lb_users;
+
+    private System.Windows.Forms.Label lb_info;
+
+    private System.Windows.Forms.Panel p_otherUsers;
+
+    private System.Windows.Forms.Panel p_leaveButton;
+
+    private System.Windows.Forms.Panel p_topBar;
+
+    private System.Windows.Forms.Panel p_userInfo;
+
+    private System.Windows.Forms.Panel p_rightBar;
+
+    private System.Windows.Forms.Panel p_chat;
+
+    private System.Windows.Forms.Panel p_bottomBar;
+
+    #endregion
+}
